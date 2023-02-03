1a2a3bdf9a1b5eb5cd2f8200810e410caacd413f
ginoongflores <florespaul161@gmail.com>
2023-02-03 18:46:18 +0800

    initalize setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 
-import { Task } from "./Task";
-
 function App() {
-	const [todoList, setTodoList] = useState([]);
-	const [newTask, setNewTask] = useState("");
+	const [count, setCount] = useState(0);
 
-	const handleChange = (event) => {
-		setNewTask(event.target.value);
+	const increment = () => {
+		setCount(count + 1);
 	};
 
-	const addTasks = () => {
-		const tasks = {
-			id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-			taskName: newTask,
-			complete: false,
-		};
-		setTodoList(tasks.taskName !== "" ? [...todoList, tasks] : todoList);
-	};
-
-	const deleteTask = (id) => {
-		setTodoList(todoList.filter((task) => task.id !== id));
-	};
-
-	const completeTask = (id) => {
-		setTodoList(
-			todoList.map((task) => {
-				if (task.id === id) {
-					return { ...task, complete: true };
-				} else {
-					return task;
-				}
-			})
-		);
+	const decrement = () => {
+		setCount(count - 1);
 	};
 
 	return (
 		<div className="App">
-			<div className="todoList">
-				<div className="addTask">
-					<input type="text" onChange={handleChange} />
-					<button onClick={addTasks}>Add a task</button>
-				</div>
-
-				<h2 className="tasks">Added tasks</h2>
-				{todoList.map((task) => {
-					return (
-						<Task
-							id={task.id}
-							taskName={task.taskName}
-							complete={task.complete}
-							completeTaskBtn={completeTask}
-							deleteTaskBtn={deleteTask}
-						/>
-					);
-				})}
-			</div>
+			<h1>Count me in: </h1>
+			<h2>{count}</h2>
+			<button>INCREMENT</button>
+			<button>DECREMENT</button>
 		</div>
 	);
 }
