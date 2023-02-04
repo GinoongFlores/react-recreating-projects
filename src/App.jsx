0a440185c1d04b7975e8659f9bec54b40c3dbe7f
ginoongflores <florespaul161@gmail.com>
2023-02-04 14:31:10 +0800

    removed annotation
    this removes the all the comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ function App() {
 	const [count, setCount] = useState(0);
 	const [counter, setCounter] = useState("");
 
-	// the addAll array will store all the previous entries and adds them up
 	const [addAll, setAddAll] = useState([]);
 
-	// addOnc boolean will prevent the add button from adding the same entry twice
 	const [addOnce, setAddOnce] = useState(false);
 
 	const increment = () => {
@@ -18,17 +16,11 @@ function App() {
 
 	const decrement = () => {
 		setCount(count - 1);
-		count === 0 && setCount(0); // simplified version using ternary operator
+		count === 0 && setCount(0); 
 	};
 
 	const save = () => {
 		count !== 0 && setCounter(counter + count + "-");
-		// if (count !== 0) {
-		// 	const updatedCounter = counter.endsWith("-")
-		// 		? counter + count
-		// 		: counter + count + "-";
-		// 	setCounter(updatedCounter);
-		// }
 	};
 
 	const add = () => {
