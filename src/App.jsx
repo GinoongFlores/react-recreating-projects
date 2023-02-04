0fb53a8553e5057fb8eda9a6ff68eb4a5af67155
ginoongflores <florespaul161@gmail.com>
2023-02-04 12:20:52 +0800

    comments
    added for readability on

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [counter, setCounter] = useState("");
+
+	// the addAll array will store all the previous entries and adds them up
+	const [addAll, setAddAll] = useState([]);
+
+	// addOnc boolean will prevent the add button from adding the same entry twice
+	const [addOnce, setAddOnce] = useState(false);
 
 	const increment = () => {
 		setCount(count + 1);
