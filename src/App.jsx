3ed28b53d669d25f417ecf3c38fc773060d982b8
ginoongflores <florespaul161@gmail.com>
2023-02-04 12:22:21 +0800

    feat(save&add): function

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,54 @@ function App() {
 
 	const decrement = () => {
 		setCount(count - 1);
+		count === 0 && setCount(0); // simplified version using ternary operator
+	};
+
+	const save = () => {
+		// count !== 0 && setCounter(counter + count + "-");
+		if (count !== 0) {
+			const updatedCounter = counter.endsWith("-")
+				? counter + count
+				: counter + "-" + count;
+			setCounter(updatedCounter);
+		}
+	};
+
+	const add = () => {
+		if (!addOnce && count !== 0 && counter.split("-").length > 1) {
+			setAddAll([
+				...addAll,
+				counter
+					.split("-")
+					.map(Number)
+					.reduce((accumulator, currentValue) => accumulator + currentValue),
+			]);
+
+			setAddOnce(true);
+		}
+	};
+
+	const reset = () => {
+		setCount(0);
+		setCounter(0);
+		setAddAll([]);
+		setAddOnce(false);
 	};
 
 	return (
 		<div className="App">
 			<h1>Count me in: </h1>
 			<h2>{count}</h2>
-			<button>INCREMENT</button>
-			<button>DECREMENT</button>
+			<div className="buttons">
+				<button onClick={increment}>INCREMENT</button>
+				<button onClick={decrement}>DECREMENT</button>
+				<button onClick={save}>SAVE</button>
+				<button onClick={reset}>RESET</button>
+				<button onClick={add}>ADD</button>
+			</div>
+
+			<h3>Previous entries: {counter}</h3>
+			<h3>Sum: {addAll}</h3>
 		</div>
 	);
 }
