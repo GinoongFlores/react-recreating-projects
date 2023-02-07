8a0dcccd005d3c34723ba2fb56cbe9003bdd970b
ginoongflores <florespaul161@gmail.com>
2023-02-07 13:21:41 +0800

    freshed environment

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	const increment = () => {
-		setCount(count + 1);
-	};
-
-	const decrement = () => {
-		setCount(count - 1);
-	};
-
-	return (
-		<div className="App">
-			<h1>Count me in: </h1>
-			<h2>{count}</h2>
-			<button>INCREMENT</button>
-			<button>DECREMENT</button>
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;
